c49df3d6c863f2c4d9a6d422a2ec047852e3e796
agastyajain <71954407+agastyajain@users.noreply.github.com>
2020-11-28 12:51:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+var number=5;
 
 function preload() {
 	helicopterIMG = loadImage("helicopter.png");
@@ -46,6 +47,10 @@ function setup() {
 function draw() {
 	rectMode(CENTER);
 	background(0);
+	textSize(20);
+	fill("white")
+	stroke("grey");
+	text("CHANCES LEFT : "+number,200,20 );
 
 	if (keyDown("left")) {
 		helicopterSprite.x = helicopterSprite.x - 10;
@@ -53,24 +58,45 @@ function draw() {
 	if (keyDown("right")) {
 		helicopterSprite.x = helicopterSprite.x + 10;
 	}
-	
-	keyPressed();
+
+	if (keyWentDown("down")&&(number>0)) {
+		pack_options = { restitution: 1.0 }
+		packageSprite = createSprite(helicopterSprite.x, 100, 10, 10, pack_options);
+		packageSprite.addImage(packageIMG)
+		packageSprite.scale = 0.2
+		packageSprite.velocityY = 5;
+		number=number-1;
+	}
+
+	if(number==0){
+		number="GAME OVER!!!";
+	}
+
+
+
+
+
+
 	drawSprites();
 
 
 }
 
-function keyPressed() {
-	if (keyWentDown("down")) {
-		pack_options = { restitution: 1.0 }
-		packageSprite = createSprite(width / 2, 100, 10, 10, pack_options);
-		packageSprite.addImage(packageIMG)
-		packageSprite.scale = 0.2
-		packageSprite.velocityY = 5;
-		
+
+function isTouching(object1, object2) {
+	if (
+		(object1.x - object2.x < object1.width / 2 + object2.width / 2) &&
+		(object2.x - object1.x < object1.width / 2 + object2.width / 2) &&
+		(object1.y - object2.y < object1.height / 2 + object2.height / 2) &&
+		(object2.y - object1.y < object1.height / 2 + object2.height / 2)
+	) {
+		return true;
+	}
+	else {
+		return false;
 
 	}
-
 }
+
 
 
